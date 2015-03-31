515ed75c7ad26bed86a7786159ab20a46e1c7e72
Christopher Warren <christopher.warren@gmail.com>
2015-03-31 13:46:28 -0500

    add coveralls

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in doorkeeper-jwt.gemspec
 gemspec
+
+gem 'coveralls', require: false
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,6 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'doorkeeper-jwt'
 require 'pry'
+
+require 'coveralls'
+Coveralls.wear!
